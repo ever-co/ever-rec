c5839a1d336ce6bcb66aaec8ceacd3b1ab78aa0f
Anishali2 <ansihali1221@gmail.com>
2025-05-14 05:08:55 +0500

    fixed the header in extension also

--- apps/extensions/src/content/panel/screens/IntegrationsScreen/IntegrationsScreen.tsx
+++ apps/extensions/src/content/panel/screens/IntegrationsScreen/IntegrationsScreen.tsx
@@ -6,11 +6,19 @@ import DropboxPage from '../imagesScreen/pages/integrations/DropboxPage';
 import JiraPage from '../imagesScreen/pages/integrations/JiraPage';
 import SlackPage from '../imagesScreen/pages/integrations/SlackPage';
 import TrelloPage from '../imagesScreen/pages/integrations/TrelloPage';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import { IUser } from '@/app/interfaces/IUserData';
+import SCHeader from '../../shared/SCHeader/SCHeader';
 
 const IntegrationsScreen = () => {
+  const user: IUser = useSelector((state: RootStateOrAny) => state.auth.user);
   return (
     <DashboardCard className={styles.mainWrapper}>
-      <div className={styles.mainHeader}>Integrations</div>
+      <SCHeader
+        text={'Integrations'}
+        userPhotoURL={user?.photoURL}
+        showSearch={false}
+      />{' '}
       <div className={classNames(styles.innerWrapper, 'scroll-div')}>
         <DrivePage />
         <DropboxPage />
