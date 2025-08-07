e14c4c8f12cb7b45c95cd9d29e84d771244799c3
adkif <akifungo@gmail.com>
2025-08-07 11:37:58 +0200

    style(mux): reorder imports
    Align import order with project conventions.

--- apps/api/src/module/video-services/mux/mux.module.ts
+++ apps/api/src/module/video-services/mux/mux.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from 'nestjs-http-promise';
+import { AuthModule } from '../../../module/auth/auth.module';
 import { MuxController } from './mux.controller';
 import { MuxService } from './mux.service';
-import { AuthModule } from '../../../module/auth/auth.module';
 
 @Module({
   imports: [HttpModule, AuthModule],
