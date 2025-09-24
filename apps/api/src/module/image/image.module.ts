96105550ff12261fe92a5fe3cc45ac74036b54e8
adkif <akifungo@gmail.com>
2025-09-24 11:52:24 +0200

    refactor(image): import shared module
    Consolidate shared service provisioning.
    This replaces the direct provision of SharedService within the ImageModule
    with an import of SharedModule, enhancing modularity and encapsulation.

--- apps/api/src/module/image/image.module.ts
+++ apps/api/src/module/image/image.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from 'nestjs-http-promise';
 import { FirebaseClient } from 'src/services/firebase/firebase.client';
 import { GoogleApis } from 'src/services/googleapis/google.apis';
+import { SharedModule } from 'src/services/shared/shared.module';
 import { UniqueViewsSharedService } from 'src/services/shared/uniqueViews.shared.service';
 import { FoldersSharedService } from '../../services/shared/folders.shared.service';
-import { SharedService } from '../../services/shared/shared.service';
 import { AuthModule } from '../auth/auth.module';
 import { EditorWebsocketModule } from '../editor-websocket-module/editor-websocket.module';
 import { GauzyModule } from '../gauzy';
@@ -15,12 +15,17 @@ import { FirebaseImageUploader } from './uploader/firebase-image.upload';
 import { GauzyImageUploader } from './uploader/gauzy-image.upload';
 
 @Module({
-  imports: [HttpModule, EditorWebsocketModule, GauzyModule, AuthModule],
+  imports: [
+    HttpModule,
+    EditorWebsocketModule,
+    GauzyModule,
+    AuthModule,
+    SharedModule,
+  ],
   providers: [
     ImageService,
     FirebaseClient,
     GoogleApis,
-    SharedService,
     FoldersSharedService,
     UniqueViewsSharedService,
     FirebaseImageUploader,
